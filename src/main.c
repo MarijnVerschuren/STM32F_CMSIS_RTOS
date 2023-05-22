1960517908fb9c7bf07c2832b2f73fbba0a695a5
MarijnVerschuren <marijnverschuren3@gmail.com>
2023-05-22 22:21:53 +0200

    Removed redundant code

--- src/main.c
+++ src/main.c
@@ -26,41 +26,30 @@
 #define BTN_PIN 0
 
 
+/* variables */
+io_buffer_t* uart_buf;
+TaskHandle_t* idle_task;
+TaskHandle_t* watchdog_task;
+
+
 /* CMSIS */
 extern void TIM1_UP_TIM10_IRQHandler(void) {
 	TIM10->SR &= ~TIM_SR_UIF;
-	// GPIO_toggle(LED_GPIO_PORT, LED_PIN);
 }
 extern void EXTI0_IRQHandler(void) {
 	EXTI->PR = EXTI_PR_PR0;
-	// GPIO_toggle(LED_GPIO_PORT, LED_PIN);
 }
 
 
 /* RTOS */
-uint32_t idle_cnt = 0;
-uint32_t task_cnt = 0;
-
-void idle(void* args) {
-	uint32_t prev = tick;
+void idle(void* memory_pool) {
 	for (;;) {
-		if (tick - prev > 1000) {
-			prev = tick;
-			idle_cnt++;
-			GPIO_toggle(LED_GPIO_PORT, LED_PIN);
-		}
-		taskYIELD();
+
 	}
 }
-void task(void* args) {
-	uint64_t prev = tick;
+void watchdog(void* memory_pool) {
 	for (;;) {
-		if (tick - prev > 100) {
-			prev = tick;
-			task_cnt++;
-			GPIO_toggle(LED_GPIO_PORT, LED_PIN);
-		}
-		taskYIELD();
+		reset_watchdog();
 	}
 }
 
@@ -89,7 +78,7 @@ int main(void) {
 	enable_CRC();  // polynomial: 0x4C11DB7
 
 	// UART input
-	io_buffer_t* uart_buf = new_buffer(1024);
+	uart_buf = new_buffer(1024);
 	if (!uart_buf) { return -1; }  // allocation error
 	config_UART(USART1_TX_A9, USART1_RX_A10, 115200);
 	start_USART_read_irq(USART1, uart_buf, 1);
@@ -99,53 +88,31 @@ int main(void) {
 	start_TIM_update_irq(TIM10);  // TIM1_UP_TIM10_IRQHandler
 	start_TIM(TIM10);
 
-	// PWM output
-	config_PWM(TIM4_CH4_B9, 100, 20000); TIM4->CCR4 = 550;
-
-	// RNG generator
-	start_RNG();
-
-	// I2C
-	config_I2C(I2C1_SCL_B6, I2C1_SDA_B7, 0);
-
-	// S0S90 external counter
-	config_encoder_S0S90(TIM2_CH1_A0, TIM2_CH2_A1);
-	start_encoder_S0S90(TIM2);
-
 	// watchdog
 	config_watchdog(0, 0xfff);  // 512 ms
-	//start_watchdog();
+	start_watchdog();
 
 	/* RTOS */
 	if (xTaskCreate(
 		idle,
-		"task idle",
-		40,			// stack size in words (160 B)
-		NULL,		// stack parameter
+		"idle",
+		configMINIMAL_STACK_SIZE,	// stack size in words (160 B)
+		NULL,						// stack parameter
 		(tskIDLE_PRIORITY | portPRIVILEGE_BIT),
-		NULL
+		idle_task
 	) != pdPASS) {
 		__NOP();
 	}
-
 	if (xTaskCreate(
-		task,
-		"task",
-		40,			// stack size in words (160 B)
-		NULL,		// stack parameter
+		watchdog,
+		"watchdog",
+		configMINIMAL_STACK_SIZE,	// stack size in words (160 B)
+		NULL,						// stack parameter
 		(tskIDLE_PRIORITY | portPRIVILEGE_BIT),
-		NULL
+		watchdog_task
 	) != pdPASS) {
 		__NOP();
 	}
-	/*
-	TaskFunction_t pxTaskCode,
-	const char * const pcName,
-	const configSTACK_DEPTH_TYPE usStackDepth,
-	void * const pvParameters,
-	UBaseType_t uxPriority,
-	TaskHandle_t * const pxCreatedTask
-	 */
 
 	// start scheduler
 	vTaskStartScheduler();
@@ -153,9 +120,4 @@ int main(void) {
 	return 0;
 }
 
-
-#elif defined(STM32F3xx)
-/* STM32F3xx:
- * 	_WFI(); works here without any additional config
- */
 #endif
--- src/rtos.c
+++ src/rtos.c
@@ -16,40 +16,16 @@ void vApplicationStackOverflowHook(TaskHandle_t xTask, char* pcTaskName) {
 	for(;;);
 }
 void vApplicationGetTimerTaskMemory(StaticTask_t** ppxTimerTaskTCBBuffer, StackType_t** ppxTimerTaskStackBuffer, uint32_t* pulTimerTaskStackSize) {
-	/* If the buffers to be provided to the Timer task are declared inside this
-	 * function then they must be declared static - otherwise they will be allocated on
-	 * the stack and so not exists after this function exits. */
-	static StaticTask_t xTimerTaskTCB;
-	static StackType_t uxTimerTaskStack[ configTIMER_TASK_STACK_DEPTH ];
-
-	/* Pass out a pointer to the StaticTask_t structure in which the Idle
-	 * task's state will be stored. */
-	*ppxTimerTaskTCBBuffer = &xTimerTaskTCB;
-
-	/* Pass out the array that will be used as the Timer task's stack. */
-	*ppxTimerTaskStackBuffer = uxTimerTaskStack;
-
-	/* Pass out the size of the array pointed to by *ppxTimerTaskStackBuffer.
-	 * Note that, as the array is necessarily of type StackType_t,
-	 * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
-	*pulTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH;
+	static StaticTask_t xTimerTaskTCB;						// all created buffers need to be static because they are used after function lifetime
+	static StackType_t uxTimerTaskStack[configTIMER_TASK_STACK_DEPTH];
+	*ppxTimerTaskTCBBuffer = &xTimerTaskTCB;				// pass StaticTask_t structure in which the idle task state is stored
+	*ppxTimerTaskStackBuffer = uxTimerTaskStack;			// pass pointer to the timer stack
+	*pulTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH;	// pass the size of the timer stack in words
 }
 void vApplicationGetIdleTaskMemory(StaticTask_t** ppxIdleTaskTCBBuffer, StackType_t** ppxIdleTaskStackBuffer, uint32_t* pulIdleTaskStackSize) {
-	/* If the buffers to be provided to the Idle task are declared inside this
-	 * function then they must be declared static - otherwise they will be allocated on
-	 * the stack and so not exists after this function exits. */
-	static StaticTask_t xIdleTaskTCB;
-	static StackType_t uxIdleTaskStack[ configMINIMAL_STACK_SIZE ];
-
-	/* Pass out a pointer to the StaticTask_t structure in which the Idle
-	 * task's state will be stored. */
-	*ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
-
-	/* Pass out the array that will be used as the Idle task's stack. */
-	*ppxIdleTaskStackBuffer = uxIdleTaskStack;
-
-	/* Pass out the size of the array pointed to by *ppxIdleTaskStackBuffer.
-	 * Note that, as the array is necessarily of type StackType_t,
-	 * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
-	*pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+	static StaticTask_t xIdleTaskTCB;						// all created buffers need to be static because they are used after function lifetime
+	static StackType_t uxIdleTaskStack[configMINIMAL_STACK_SIZE];
+	*ppxIdleTaskTCBBuffer = &xIdleTaskTCB;					// pass StaticTask_t structure in which the idle task state is stored
+	*ppxIdleTaskStackBuffer = uxIdleTaskStack;				// pass pointer to the idle task stack
+	*pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;		// pass the size of the idle task stack in words
 }
